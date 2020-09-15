9458634f7bc8006000968c3fe196415f9c7eb35d
Mitchell Duhe <mitchellduhe@gmail.com>
2020-09-15 13:27:27 -0500

    import wordblock

--- src/routes/Barplot.svelte
+++ src/routes/Barplot.svelte
@@ -2,7 +2,7 @@
 	import { onMount,createEventDispatcher,tick } from 'svelte'
 	const dispatch = createEventDispatcher();
 	import Loading from './Loading.svelte'
-	// import Wordblock from './Wordblock.svelte'
+	import Wordblock from './Wordblock.svelte'
 	import Bar from './Bar.svelte';
 	import { userTweetData } from './userTweetData.js'
 
