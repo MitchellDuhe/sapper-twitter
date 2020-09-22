882b5a3c3bce072cba6a2bcf51e0bc2ece636f76
Mitchell Duhe <mitchellduhe@gmail.com>
2020-09-21 22:16:36 -0500

    trying to contain word groups in FO body

--- src/routes/Testblock.svelte
+++ src/routes/Testblock.svelte
@@ -34,6 +34,13 @@
   </foreignObject>
 
 <style>
+  body{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width:100%;
+    height:100%;    
+  }
   .word-groups-inner-container{
 
     position: fixed;
